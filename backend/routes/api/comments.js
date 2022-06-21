1875475a91f5f0b5e2cabe2c979de2cc2c2f2337
Flora Kho <florakho123@gmail.com>
2022-06-21 11:32:53 -0700

    photoReducer

---
+++ backend/routes/api/comments.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+const { requireAuth, restoreUser } = require('../../utils/auth');
+const { Comment, User } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+
+const validateComments = [
+    check('content')
+        .notEmpty()
+        .withMessage('Please enter your comment.'),
+    handleValidationErrors
+];
+
+
+//create a comment (user only)
+router.post('/', requireAuth, restoreUser, asyncHandler(async (req, res) => {
+    const { content, photoId, userId } = req.body;
+    const newComment = await Comment.create({
+        content: content,
+        userId: userId,
+        photoId: photoId
+    })
+    return res.json(newComment);
+}))
+
+//read all comment
+router.get('/photos/:photoId', asyncHandler(async (req, res) => {
+    const { photoId } = req.params;
+    const comments = await Comment.findAll({
+        where: { photoId: photoId },
+        include: User
+    });
+    return res.json(comments);
+}))
+
+//update comment (user only)
+router.put('/:commentId', requireAuth, restoreUser, asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    const oldComment = await Comment.findByPk(commentId);
+    const { content } = req.body;
+    const updatedComment = await oldComment.update({
+        content
+    })
+    return res.json(updatedComment);
+}))
+
+//delete comment (user only)
+router.delete('/:commentId', requireAuth, asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+
+    const deleteComment = await Comment.findByPk(commentId);
+    await deleteComment.destroy();
+    return res.json(deleteComment);
+}))
+
+
+module.exports = router;
---
+++ frontend/src/store/photos.js
@@ -0,0 +1,149 @@
+import { csrfFetch } from "./csrf";
+
+const ADD_PHOTO = 'photos/ADD_PHOTO';
+const GET_PHOTO = 'photos/GET_PHOTO';
+const GET_ALL_PHOTOS = 'photos/GET_ALL_PHOTOS';
+const EDIT_PHOTO = 'photos/EDIT_PHOTO';
+const DELETE_PHOTO = 'photos/DELETE_PHOTO';
+
+
+//action creators
+const addPhoto = (photo) => {
+    return {
+        type: ADD_PHOTO,
+        photo
+    }
+}
+
+const getPhoto = (photo) => {
+    return {
+        type: GET_PHOTO,
+        photo
+    }
+}
+
+const getAllPhotos = (photos) => {
+    return {
+        type: GET_ALL_PHOTOS,
+        photos
+    }
+}
+
+const editPhoto = (photo) => {
+    return {
+        type: EDIT_PHOTO,
+        photo
+    }
+}
+
+const deletePhoto = (photoId) => {
+    return {
+        type: DELETE_PHOTO,
+        photoId
+    }
+}
+
+//thunk
+//create
+export const uploadPhoto = (photo) => async (dispatch) => {
+    const response = await csrfFetch(`/api/photos`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(photo),
+    });
+
+    if (response.ok) {
+        const newPhoto = await response.json();
+        dispatch(addPhoto(newPhoto));
+        return newPhoto;
+    }
+}
+
+//read
+export const readPhoto = (photoId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/photos/${photoId}`);
+
+    if (response.ok) {
+        const photo = await response.json();
+        dispatch(getPhoto(photo));
+    }
+}
+
+//read
+export const readAllPhoto = (userId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/users/${userId}/photos`);
+    if (response.ok) {
+        const photos = await response.json();
+        dispatch(getAllPhotos(photos));
+    }
+}
+
+//update
+export const updatePhoto = (photo) => async (dispatch) => {
+    const response = await csrfFetch(`/api/photos/${photo.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(photo)
+    })
+    if (response.ok) {
+        const newPhotoInfo = await response.json();
+        dispatch(editPhoto(newPhotoInfo));
+        return newPhotoInfo;
+    }
+}
+
+//delete
+export const removePhoto = (photoId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/photos/${photoId}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        dispatch(deletePhoto(photoId));
+        return photoId;
+    }
+}
+
+
+//reducer
+const initialState = {};
+
+const photoReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case ADD_PHOTO:
+            return {
+                ...state,
+                [action.photo.id]: action.photo
+            };
+
+        case GET_PHOTO:
+            newState = { ...state };
+            newState[action.photo.id] = action.photo;
+            return newState;
+
+        case GET_ALL_PHOTOS:
+            newState = {};
+            action.photos.forEach(photo => {
+                newState[photo.id] = photo;
+            });
+            return { ...state, ...newState };
+
+        case EDIT_PHOTO:
+            return {
+                ...state,
+                [action.photo.id]: action.photo
+            };
+
+        case DELETE_PHOTO:
+            newState = { ...state }
+            delete newState[action.photoId];
+            return newState;
+
+        default:
+            return state;
+    }
+}
+
+export default photoReducer;
