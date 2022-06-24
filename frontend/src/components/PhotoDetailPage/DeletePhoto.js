5bfb4bf71ea41d909359e0ce3f0f2b53d0a316a5
Flora Kho <florakho123@gmail.com>
2022-06-24 15:39:37 -0700

    delete

--- frontend/src/components/PhotoDetailPage/DeletePhoto.js
+++ frontend/src/components/PhotoDetailPage/DeletePhoto.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { removePhoto } from '../../store/photos';
+import './DeletePhoto.css';
 
 function DeletePhoto ({photo}) {
+    const [showModal, setShowModal] = useState(false);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleDeleteSubmit = async (e) => {
+        e.preventDefault();
+        dispatch(removePhoto(photo.id))
+        setShowModal(false);
+        history.push(`/photostream`);
+    }
+
     return (
         <>
-            <button>
-                <i className="fa-solid fa-trash-can"></i>
-            </button>
+            <div className='delete-btn'>
+                <h2 onClick={() => setShowModal(true)}><i className="fa-solid fa-trash-can"></i></h2>
+            </div> 
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    {/* <div className='delete-form'> */}
+                        {/* <div className='delete-title'> */}
+                            <h2 className='modal-title'>Delete Photo</h2>
+                        {/* </div> */}
+                        {/* <div className='delete-message'> */}
+                    <p className='modal-text'>Are you sure you want to delete this photo?</p>
+                        {/* </div> */}
+                        <div className='modal-button'>
+                            <button className='modal-button-cancel' onClick={() => setShowModal(false)}>Cancel</button>
+                            <button className='modal-button-confirm' onClick={handleDeleteSubmit}>Delete</button>
+                        </div>
+                    {/* </div> */}
+                </Modal>
+            )}
         </>
     )
 }
--- frontend/src/components/PhotoDetailPage/index.js
+++ frontend/src/components/PhotoDetailPage/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { readPhoto, updatePhoto } from '../../store/photos';
 import DeletePhoto from './DeletePhoto';
+import './PhotoDetailPage.css';
 
 
 function PhotoDetailPage() {
@@ -13,8 +14,6 @@ function PhotoDetailPage() {
     const sessionUser = useSelector((state) => state.session.user);
     const photo = useSelector((state) => state.photos[photoId]);
 
-    console.log('Here is photo object:', photo);
-
     const currentTitle = photo?.title;
     const currentDescription = photo?.description;
 
@@ -22,7 +21,6 @@ function PhotoDetailPage() {
     const [title, setTitle] = useState(currentTitle);
     const [description, setDiscription] = useState(currentDescription);
     const [errors, setErrors] = useState([]);
-
 
     useEffect(() => {
         history.push(`/photos/${photoId}`);
@@ -62,66 +60,82 @@ function PhotoDetailPage() {
     }
 
 
-    if(!sessionUser || !photo) return (
+    if (!sessionUser || !photo) return (
         <p className='loading'><i className="fa-solid fa-spinner"></i> Loading...</p>
     )
 
     return (
         <>
-        <div className='photodetailpage'>
-            <div className='photodetail-container'>
-                <img src={photo?.imgURL} alt={photo.title} />
-            </div>
-            <div className='photodetail-delete-fav'>
-                {sessionUser?.id === photo?.userId ? (
-                    <>
-                        <DeletePhoto photo={photo} />
-                    </>
-                ) : <p><i class="fa-regular fa-heart"></i> FAVORITE BUTTON HERE</p>}
-            </div>
-            <div className='photodetail-edit-container'>
-                <div>
-                    {sessionUser.id === photo.userId && edit ? (
-                        <form onSubmit={handleEditSubmit}>
-                            <div className="errors">
-                                <ul>
-                                    {errors.map(error => (
-                                        <li key={error}>{error}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div>
-                                <input type='text'
-                                    value={title}
-                                    required
-                                    onChange={e => setTitle(e.target.value)}
-                                />
-                                <textarea
-                                    type='text'
-                                    value={description}
-                                    onChange={e => setDiscription(e.target.value)}
-                                    placeholder='Enter description'
+            <div className='photodetailpage'>
+                <div className='photodetail-container'>
+                    <div className='back' onClick={() => history.push('/explore')}>
+                        <h3><i className="fa-solid fa-arrow-left"></i> Back to explore</h3>
+                    </div>
+                    <div className='single-photo'>
+                        <img src={photo?.imgURL} alt={photo.title} />
+                    </div>
 
-                                />
-                                <button type='submit' disabled={!!errors.length}>Save</button>
-                            </div>
-                        </form>
-                    ) : (
-                        <div onClick={handleEditState}>
-                            <p>{currentTitle}</p>
-                            <p>{currentDescription}</p>
+                    <div className='photodetail-delete-fav'>
+                        {sessionUser?.id === photo?.userId ? (
+                            <>
+                                <DeletePhoto photo={photo} />
+                            </>
+                        ) : <p><i className="fa-regular fa-heart"></i> FAVORITE BUTTON HERE</p>}
+                    </div>
+                </div>
+
+
+                <div className='photodetail-bottom'>
+                    <div className='photodetail-left'>
+                        <div className='photodetail-edit'>
+                            <img className='avatar' src='../photos/user-1.png' alt='user_photo'/> 
+                            {sessionUser.id === photo.userId && edit ? (
+                                <form onSubmit={handleEditSubmit}>
+                                    <div className="errors">
+                                        <ul>
+                                            {errors.map(error => (
+                                                <li key={error}>{error}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <input type='text'
+                                            value={title}
+                                            required
+                                            onChange={e => setTitle(e.target.value)}
+                                        />
+                                        <textarea
+                                            type='text'
+                                            value={description}
+                                            onChange={e => setDiscription(e.target.value)}
+                                            placeholder='Enter description'
+
+                                        />
+                                        <button type='submit' disabled={!!errors.length}>Save</button>
+                                    </div>
+                                </form>
+                            ) : (
+                                <div onClick={handleEditState}>
+                                    <p>{currentTitle}</p>
+                                    <p>{currentDescription}</p>
+                                </div>
+                            )}
                         </div>
-                    )}
-                </div>
-                <div>
-                    <p>fav.length here</p>
-                    <p>comment.length here</p>
-                </div>
-                <div className='comments-part'>
-                    <p>Comment Component here</p>
+                        <div className='photodetail-add-comment'>
+                            <p>Comment Component here</p>
+                        </div>
+                    </div>
+                    <div className='photodetail-right'>
+                        <div>
+                            <p>fav.length here</p>
+                            <p>comment.length here</p>
+                        </div>
+                        <div>
+                            <p>Albums put here if we have</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
